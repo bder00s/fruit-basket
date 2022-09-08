43f180aa2a7b09eb637bb9654d18e7745282c0a4
bder00s <bonne-deroos@hotmail.com>
2022-09-08 15:58:19 +0200

    eerste opzet

--- src/App.js
+++ src/App.js
@@ -2,11 +2,88 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <h1>Fruitmand bezorgservice</h1>
-    </>
-  );
+    return (
+        <>
+            <h1>Fruitmand bezorgservice</h1>
+            <article className="aardbeien">
+                <h1>🍓 Aardbeien</h1>
+                <button
+                    type="button"
+                    id="more-button"
+                    className="more-button"
+                >
+                    +
+                </button>
+                <div className="amountOfFruit">0</div>
+                <button
+                    type="button"
+                    id="less-button"
+                    className="less-button"
+                >
+                    -
+                </button>
+
+            </article>
+
+            <article className="bananen">
+                <h1>🍌 Bananen</h1>
+                <button
+                    type="button"
+                    id="more-button"
+                    className="more-button"
+                >
+                    +
+                </button>
+                <div className="amountOfFruit">0</div>
+                <button
+                    type="button"
+                    id="less-button"
+                    className="less-button"
+                >
+                    -
+                </button>
+            </article>
+
+            <article className="appels">
+                <h1>🍏 Appels</h1>
+                <button
+                    type="button"
+                    id="more-button"
+                    className="more-button"
+                >
+                    +
+                </button>
+                <div className="amountOfFruit">0</div>
+                <button
+                    type="button"
+                    id="less-button"
+                    className="less-button"
+                >
+                    -
+                </button>
+            </article>
+
+            <article className="kiwis">
+                <h1>🥝 Kiwi's</h1>
+                <button
+                    type="button"
+                    id="more-button"
+                    className="more-button"
+                >
+                    +
+                </button>
+                <div className="amountOfFruit">0</div>
+                <button
+                    type="button"
+                    id="less-button"
+                    className="less-button"
+                >
+                    -
+                </button>
+            </article>
+
+        </>
+    );
 }
 
 export default App;
