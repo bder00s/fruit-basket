b78fee7152a94188eeb67294ccc78f94f11a9a99
bder00s <bonne-deroos@hotmail.com>
2022-09-08 16:15:15 +0200

    States toegevoegd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
+
 function App() {
+    const [fruitAmount, setFruitAmount] = React.useState(0);
+    const [addProduct, toggleAddProduct] = React.useState('');
+    const [removeProduct, toggleRemoveProduct] = React.useState('');
+    const [resetButton, toggleResetButton] = React.useState('');
+
     return (
         <>
             <h1>Fruitmand bezorgservice</h1>
@@ -11,76 +17,87 @@ function App() {
                     type="button"
                     id="more-button"
                     className="more-button"
+                    onClick={ () => toggleAddProduct(+1)}
                 >
                     +
                 </button>
-                <div className="amountOfFruit">0</div>
+                <div className="amountOfFruit">{fruitAmount}</div>
                 <button
                     type="button"
                     id="less-button"
                     className="less-button"
+                    onClick={() => toggleRemoveProduct(-1)}
                 >
                     -
                 </button>
 
             </article>
 
-            <article className="bananen">
-                <h1>🍌 Bananen</h1>
-                <button
-                    type="button"
-                    id="more-button"
-                    className="more-button"
-                >
-                    +
-                </button>
-                <div className="amountOfFruit">0</div>
-                <button
-                    type="button"
-                    id="less-button"
-                    className="less-button"
-                >
-                    -
-                </button>
-            </article>
+            {/*<article className="bananen">*/}
+            {/*    <h1>🍌 Bananen</h1>*/}
+            {/*    <button*/}
+            {/*        type="button"*/}
+            {/*        id="more-button"*/}
+            {/*        className="more-button"*/}
+            {/*    >*/}
+            {/*        +*/}
+            {/*    </button>*/}
+            {/*    <div className="amountOfFruit">0</div>*/}
+            {/*    <button*/}
+            {/*        type="button"*/}
+            {/*        id="less-button"*/}
+            {/*        className="less-button"*/}
+            {/*    >*/}
+            {/*        -*/}
+            {/*    </button>*/}
+            {/*</article>*/}
 
-            <article className="appels">
-                <h1>🍏 Appels</h1>
-                <button
-                    type="button"
-                    id="more-button"
-                    className="more-button"
-                >
-                    +
-                </button>
-                <div className="amountOfFruit">0</div>
-                <button
-                    type="button"
-                    id="less-button"
-                    className="less-button"
-                >
-                    -
-                </button>
-            </article>
+            {/*<article className="appels">*/}
+            {/*    <h1>🍏 Appels</h1>*/}
+            {/*    <button*/}
+            {/*        type="button"*/}
+            {/*        id="more-button"*/}
+            {/*        className="more-button"*/}
+            {/*    >*/}
+            {/*        +*/}
+            {/*    </button>*/}
+            {/*    <div className="amountOfFruit">0</div>*/}
+            {/*    <button*/}
+            {/*        type="button"*/}
+            {/*        id="less-button"*/}
+            {/*        className="less-button"*/}
+            {/*    >*/}
+            {/*        -*/}
+            {/*    </button>*/}
+            {/*</article>*/}
 
-            <article className="kiwis">
-                <h1>🥝 Kiwi's</h1>
-                <button
-                    type="button"
-                    id="more-button"
-                    className="more-button"
-                >
-                    +
-                </button>
-                <div className="amountOfFruit">0</div>
-                <button
-                    type="button"
-                    id="less-button"
-                    className="less-button"
-                >
-                    -
-                </button>
-            </article>
+            {/*<article className="kiwis">*/}
+            {/*    <h1>🥝 Kiwi's</h1>*/}
+            {/*    <button*/}
+            {/*        type="button"*/}
+            {/*        id="more-button"*/}
+            {/*        className="more-button"*/}
+            {/*    >*/}
+            {/*        +*/}
+            {/*    </button>*/}
+            {/*    <div className="amountOfFruit">0</div>*/}
+            {/*    <button*/}
+            {/*        type="button"*/}
+            {/*        id="less-button"*/}
+            {/*        className="less-button"*/}
+            {/*    >*/}
+            {/*        -*/}
+            {/*    </button>*/}
+            {/*</article>*/}
+
+            <button
+                className="reset-button"
+            type="button"
+            id="reset-button"
+                onClick={() => toggleResetButton(0)}
+            >
+              Reset
+            </button>
 
         </>
     );
